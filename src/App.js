1a6650101ec850bce78b61934b4d06cd1014fc2a
alanguilhermeM <alanguilherme13@outlook.com>
2023-05-22 14:32:27 -0300

    Finalizado requisito 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
@@ -9,7 +11,10 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
-        <Login />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/game" component={ Game } />
+        </Switch>
       </header>
     </div>
   );
