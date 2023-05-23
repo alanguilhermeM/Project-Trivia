d984cb3a179bcb79fe701d36b29dad2abb3964a8
Raissa Gonçalves de Oliveira <raissa@MacBook-Pro-de-Raissa.local>
2023-05-23 14:56:36 -0300

    requisito 05 corrigindo lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    const {name, gravatarEmail, score} = this.props;
-    const gravatarProfilePic = `https://www.gravatar.com/avatar/${gravatarEmail}`
-        return (
+    const { name, gravatarEmail, score } = this.props;
+    const gravatarProfilePic = `https://www.gravatar.com/avatar/${gravatarEmail}`;
+    return (
       <header>
-        <img data-testid="header-profile-picture"
-        src={gravatarProfilePic}
-        alt='profile-image'
+        <img
+          data-testid="header-profile-picture"
+          src={ gravatarProfilePic }
+          alt="gravatar-avatar"
         />
         <p data-testid="header-player-name">{name}</p>
         <strong data-testid="header-score">{score}</strong>
       </header>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({player}) => ({
-  ...player
-})
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = ({ player }) => ({
+  ...player,
+});
 
 export default connect(mapStateToProps)(Header);
